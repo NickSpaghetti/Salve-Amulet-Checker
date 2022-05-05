9b2027315a1218ecc2e5fcab5bd10ed1eddf501d
Nicholas Cicchetti <nc_cicchetti@yahoo.com>
2022-05-05 16:09:21 -0400

    Fix warnings

--- src/main/java/com/sac/managers/TobManager.java
+++ src/main/java/com/sac/managers/TobManager.java
@@ -1,7 +1,6 @@
 package com.sac.managers;
 
 import com.sac.enums.TobState;
-import net.runelite.api.Actor;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
 import net.runelite.api.Varbits;
@@ -9,12 +8,7 @@ import net.runelite.api.widgets.Widget;
 import net.runelite.client.util.Text;
 
 import javax.inject.Inject;
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
-
 import static net.runelite.api.widgets.WidgetID.TOB_GROUP_ID;
 
 public class TobManager {
@@ -31,10 +25,6 @@ public class TobManager {
 
     @Inject
     private Client client;
-
-    public HashSet<String> setRaiderNames(String[] raiderNames) {
-        return new HashSet<String>(Arrays.stream(raiderNames).filter(Objects::nonNull).collect(Collectors.toList()));
-    }
 
     public HashSet<String> getRaiderNames() {
         return tobRaiderNames;
@@ -71,17 +61,17 @@ public class TobManager {
             if (newRaidState == TobState.NoParty || newRaidState == TobState.InParty) {
                 // We're not in a raid
                 resetTobState();
-            } else if (newRaidState == TobState.InTob) {
-                tobState = TobState.InTob;
+            } else
+            {
+                tobState = newRaidState;
             }
-            tobState = newRaidState;
 
         return tobState;
     }
 
     private void resetTobState(){
         tobState = TobState.NoParty;
-        tobRaiderNames = new HashSet();
+        tobRaiderNames = new HashSet<>();
     }
 
 
